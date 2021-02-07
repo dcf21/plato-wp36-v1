a4b787682ed08ce0fe71ff3d703d88c95aac2158
Dominic Ford <dcf21-github@dcford.org.uk>
2021-02-07 18:00:46 +0000

    Add resource requests to IRIS deployment

--- kubernetes/iris/plato-shell.sh
+++ kubernetes/iris/plato-shell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kubectl exec -it `kubectl get pods --field-selector="status.phase=Running" --no-headers -o custom-columns=":metadata.name" | grep plato` -- /bin/bash
+kubectl exec -it `kubectl get pods --field-selector="status.phase=Running" --no-headers -o custom-columns=":metadata.name" | grep plato | head -n 1` -- /bin/bash
 
